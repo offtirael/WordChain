35fff092b2a281deacea1ff7f66684aecc238f4d
offtirael <offtirael@gmail.com>
2014-03-08 23:52:10 +0400

    Some minor changes

--- test.py
+++ test.py
@@ -2,15 +2,26 @@
 #-*- coding: utf-8 -*-
  
 import sys
-from PyQt5 import QtWidgets
- 
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+
+class DemoWind(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setGeometry(300, 300, 200, 200)
+        self.setWindowTitle('Demo window')
+        quit = QtGui.QPushButton('Close', self)
+        quit.setGeometry(10, 10, 70, 40)
+        self.connect(quit, QtCore.SIGNAL('clicked()'), QtGui.qApp,
+                     QtCore.slot('quit()'))
+
+
 def main():
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    button = QtWidgets.QPushButton("Hello, PyQt!")
-    window.setCentralWidget(button)
-    window.show()
-    app.exec_()
+    app = QtGui.QGuiApplication(sys.argv)
+    dw = DemoWind()
+    dw.show()
+    sys.exit(app.exec_())
+
  
 if __name__ == '__main__':
     main()

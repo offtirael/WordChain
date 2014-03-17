69de2ce821165281ee489cb4c391160b802c71a7
offtirael <offtirael@gmail.com>
2014-03-18 00:41:25 +0400

    New UI

--- ElementEditor.py
+++ ElementEditor.py
@@ -6,13 +6,61 @@ class ElementEditorWindow(QMainWindow):
     def __init__(self, parent=None):
         super(ElementEditorWindow, self).__init__(parent)
 
-        self.label1 = QLabel("TODO: Element editor")
+        # Menu options
+        self.fileMenu = QMenu("&File", self)
+        self.openAction = self.fileMenu.addAction("&Open...")
+        #openAction.setShortcut("Ctrl+O")
+        self.saveAction = self.fileMenu.addAction("&Save As...")
+        #saveAction.setShortcut("Ctrl+S")
+        self.quitAction = self.fileMenu.addAction("Exit")
+        #quitAction.setShortcut("Ctrl+Q")
 
-        self.layout1 = QVBoxLayout()
-        self.layout1.addWidget(self.label1)
+        self.menuBar().addMenu(self.fileMenu)
 
-        self.cWidget = QWidget()
-        self.cWidget.setLayout(self.layout1)
-        self.setCentralWidget(self.cWidget)
+        # Groupboxes
+        self.gBox1 = QFrame()
+        self.gBox1.setMaximumSize(QSize(200, 600))
+        self.gBox2 = QFrame()
+        self.gBox2.setMaximumSize(QSize(200, 200))
+
+        # Graphics view
+        self.graphicsView = QGraphicsView()
+        self.graphicsView.setMinimumSize(QSize(600, 600))
+
+        # Right menu
+        self.label1 = QLabel("Element name")
+        self.nameEdit = QLineEdit()
+
+        self.label2 = QLabel("Left connector")
+        self.leftConnectorCombo = QComboBox()
+        self.leftConnectorCombo.addItem("Type 1")
+        self.leftConnectorCombo.addItem("Type 2")
+        self.leftConnectorCombo.addItem("Type 3")
+        self.leftConnectorCombo.addItem("Type 4")
+
+        self.label3 = QLabel("Right connector")
+        self.rightConnectorCombo = QComboBox()
+        self.rightConnectorCombo.addItem("Type 1")
+        self.rightConnectorCombo.addItem("Type 2")
+        self.rightConnectorCombo.addItem("Type 3")
+        self.rightConnectorCombo.addItem("Type 4")
+
+        self.rightLayout = QVBoxLayout(self.gBox2)
+        self.rightLayout.addWidget(self.label1)
+        self.rightLayout.addWidget(self.nameEdit)
+        self.rightLayout.addWidget(self.label2)
+        self.rightLayout.addWidget(self.leftConnectorCombo)
+        self.rightLayout.addWidget(self.label3)
+        self.rightLayout.addWidget(self.rightConnectorCombo)
+
+        # Layout
+        self.centralWidget = QSplitter()
+        self.centralWidget.addWidget(self.gBox1)
+        self.centralWidget.addWidget(self.graphicsView)
+        self.centralWidget.addWidget(self.gBox2)
+
+        self.setCentralWidget(self.centralWidget)
+
+        self.resize(1000, 600)
 
         self.setWindowTitle("WordChain : Element editor")
